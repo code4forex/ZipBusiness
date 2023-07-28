539b76b38092bad4d9f3500b4557c0d9773262df
Jeremiah Sanders <108173608+code4forex@users.noreply.github.com>
2023-07-28 02:28:38 -0500

    v1

--- businessname.py
+++ businessname.py
@@ -1,15 +1,37 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 import time
+import os
+import shutil
 
-def run(playwright: Playwright) -> None:
-    user_data_dir = "/var/folders/7h/mfz36wt95qs6cgvsgn14k7tw0000gn/T/tmp0x0ug69f"  # Replace with your desired path
+
+def get_proxy_info():
+    proxy_server = os.getenv('PROXY_SERVER')
+    proxy_username = os.getenv('PROXY_USERNAME')
+    proxy_password = os.getenv('PROXY_PASSWORD')
+    
+    if proxy_server and proxy_username and proxy_password:
+        return {
+            'server': f'http://{proxy_username}:{proxy_password}@{proxy_server}',
+        }
+    else:
+        return None
+
+def run(playwright: sync_playwright) -> None:
+    user_data_dir = "/var/folders/7h/mfz36wt95qs6cgvsgn14k7tw0000gn/T/tmp0x0ug69f"
+    shutil.rmtree(user_data_dir, ignore_errors=True)
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"
+
+    # Set proxy information
+    proxy = get_proxy_info()
 
     browser = playwright.chromium.launch_persistent_context(
         user_data_dir=user_data_dir,
         headless=False,
-        args=[f"--user-agent={user_agent}"]
+        args=[f"--user-agent={user_agent}"],
+        proxy=proxy 
     )
+
+
     context = browser.new_page()  # Create a new page within the persistent context
     context.goto("https://apps.ilsos.gov/businessentitysearch/")
     context.wait_for_load_state("networkidle")
@@ -17,13 +39,18 @@ def run(playwright: Playwright) -> None:
     context.click("#searchValue")
     context.wait_for_load_state("networkidle")
     context.fill("#searchValue", "brainfuseaii")
-    context.wait_for_load_state("networkidle")
-    context.click('button:has-text("Submit")')
+    time.sleep(2)
+
+    # Use JavaScript to click the button
+    context.evaluate('''() => {
+        const submitButton = document.querySelector("#report-corp > div > div.cta > div > div > input");
+        submitButton.click();
+    }''')
 
     time.sleep(4)
     # ---------------------
-    ##context.close()
-    ##browser.close()
+    context.close()
+    browser.close()
 
 with sync_playwright() as playwright:
     run(playwright)
--- businessselinum.py
+++ businessselinum.py
@@ -1,17 +1,23 @@
+import random
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 def run():
     user_data_dir = "/var/folders/7h/mfz36wt95qs6cgvsgn14k7tw0000gn/T/tmp0x0ug69f"  # Replace with your desired path
 
-    # Set custom user agent
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"
+    # List of user agent strings to randomly choose from
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
+        # Add more user agents if desired
+    ]
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument(f"--user-agent={user_agent}")
+
+    # Randomly select a user agent string
+    chrome_options.add_argument(f"--user-agent={random.choice(user_agents)}")
 
     # Use the desired user data directory
     chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
@@ -23,14 +29,11 @@ def run():
         driver.get("https://apps.ilsos.gov/businessentitysearch/")
 
         # Wait for the page to load
-        time.sleep(2)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="name"]')))
 
         # Click the "Name" checkbox
-        name_checkbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="name"]')))
+        name_checkbox = driver.find_element(By.XPATH, '//*[@id="name"]')
         name_checkbox.click()
-
-        # Wait for the page to load after clicking the checkbox
-        time.sleep(2)
 
         # Fill the search input with "brainfuseaii"
         search_input = driver.find_element(By.ID, "searchValue")
@@ -39,14 +42,25 @@ def run():
         search_input.send_keys("brainfuseaii")
 
         # Wait for the page to load after filling the input
-        time.sleep(2)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="report-corp"]/div/div[3]/div/div/input')))
+
+        # Emulate a human-like typing speed
+        time.sleep(random.uniform(0.5, 1.5))
 
         # Click the "Submit" button
-        submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="report-corp"]/div/div[3]/div/div/input')))
+        submit_button = driver.find_element(By.XPATH, '//*[@id="report-corp"]/div/div[3]/div/div/input')
         submit_button.click()
 
         # Wait for the results to appear on the next page
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "results")))
+
+        # Scroll to simulate human interaction
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(random.uniform(1, 2))
+
+        # Scroll back up
+        driver.execute_script("window.scrollTo(0, 0);")
+        time.sleep(random.uniform(1, 2))
 
         # Now you can interact with elements on the results page
         # For example, you can find elements and retrieve their content
@@ -56,6 +70,10 @@ def run():
 
         # You can also interact with other elements on the page as needed
         # ...
+
+        # Simulate navigating back
+        driver.back()
+        time.sleep(random.uniform(1, 2))
 
     finally:
         # Close the browser when done
