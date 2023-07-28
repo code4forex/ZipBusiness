1c8f8c3fea754577570c15f24394026da732227f
Jeremiah Sanders <108173608+code4forex@users.noreply.github.com>
2023-07-27 22:45:45 -0500

    update business name to domain name

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
 
-api_key = "dLYcRuqYRU8Y_4veJZXF3RUZKFYxHVS7SV5"
-api_secret = "R7xf6zYJJf63D7ZGgPWMwD"
+load_dotenv()
+
+
+api_key = os.getenv("api_key")
+api_secret = os.getenv("api_secret")
 reg_headers = {
     "Authorization": f"sso-key {api_key}:{api_secret}",
     "accept": "application/json"
 }
 
+
 def get_reg_url(check_domain):
     return f"https://api.godaddy.com/v1/domains/available?domain={check_domain}&checkType=FULL&forTransfer=false"
+
 
 def check_domain_available(check_domain):
     reg_url = get_reg_url(check_domain)
@@ -27,18 +35,22 @@ def check_domain_available(check_domain):
     else:
         return {"message": f"Domain {check_domain} is not available"}, 200
 
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
+
 @app.route("/checkdomain", methods=["POST"])
 def check_domain_route():
-    domain_name = request.form.get("business_name")
+    domain_name = request.form.get("domain_name")
     if not domain_name:
         return {"message": "Domain name is required"}, 400
 
     result, status_code = check_domain_available(domain_name)
     return jsonify(result), status_code
+
+
 
 if __name__ == '__main__':
     app.run()
