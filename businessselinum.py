898a80c0dfb011d12046af3dbb88ad0a543d4c2f
Jeremiah Sanders <108173608+code4forex@users.noreply.github.com>
2023-07-31 17:00:06 -0500

    working

--- businessselinum.py
+++ businessselinum.py
@@ -12,6 +12,22 @@ def simulate_typing(driver, element, text):
         ActionChains(driver).send_keys_to_element(element, char).perform()
         time.sleep(random.uniform(0.1, 0.2))
 
+def solve_captcha(driver):
+    # Check if the "I'm not a robot" captcha is present on the page
+    if "i'm not a robot" in driver.page_source.lower():
+        # Find the captcha checkbox element
+        captcha_checkbox = driver.find_element(By.XPATH, '//span[@aria-checked="false"]')
+        
+        # Scroll into view to interact with the captcha checkbox
+        driver.execute_script("arguments[0].scrollIntoView(true);", captcha_checkbox)
+        time.sleep(random.uniform(0.5, 1))
+
+        # Click the captcha checkbox to mark it as checked
+        captcha_checkbox.click()
+        time.sleep(random.uniform(0.5, 1))
+        return True
+    return False
+
 def run():
     user_data_dir = "/var/folders/7h/mfz36wt95qs6cgvsgn14k7tw0000gn/T/tmp0x0ug69f"  # Replace with your desired path
 
@@ -53,9 +69,9 @@ def run():
         name_checkbox = driver.find_element(By.XPATH, '//*[@id="name"]')
         name_checkbox.click()
 
-        # Fill the search input with "brainfuseaii"
+        # Fill the search input with "jarul jfm llc"
         search_input = driver.find_element(By.ID, "searchValue")
-        simulate_typing(driver, search_input, "workingtimellc")
+        simulate_typing(driver, search_input, "jarul jfm llc")
 
         # Wait for the page to load after filling the input
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="report-corp"]/div/div[3]/div/div/input')))
@@ -69,6 +85,14 @@ def run():
 
         # Wait for the results to appear on the next page
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "results")))
+
+        # Check for captcha and solve it
+        captcha_solved = solve_captcha(driver)
+
+        if captcha_solved:
+            print("Captcha solved")
+        else:
+            print("No captcha")
 
         # Scroll to simulate human interaction
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
